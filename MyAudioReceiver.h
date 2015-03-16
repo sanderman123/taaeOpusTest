8eb30759964d228a24997e2288fa7b6d6d3eb8ee
Sander Valstar <sanderman123@hotmail.com>
2015-03-16 20:38:36 +0800

    first commit

---
+++ MyAudioReceiver.h
@@ -0,0 +1,13 @@
+//
+//  MyAudioReceiver.h
+//  taaeOpusTest
+//
+//  Created by Sander Valstar on 3/16/15.
+//  Copyright (c) 2015 Sander Valstar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyAudioReceiver : NSObject
+
+@end
---
+++ MyAudioReceiver.mm
@@ -0,0 +1,13 @@
+//
+//  MyAudioReceiver.m
+//  taaeOpusTest
+//
+//  Created by Sander Valstar on 3/16/15.
+//  Copyright (c) 2015 Sander Valstar. All rights reserved.
+//
+
+#import "MyAudioReceiver.h"
+
+@implementation MyAudioReceiver
+
+@end
